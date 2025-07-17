c78c50e1f317d91925fdea12ae1f5e4581265a27
Dmitrii <Vdmitr11@outlook.com>
2025-07-17 14:47:33 +0300

    Изменения: - Обернул сервисы uvicorn и locust в docker

---
+++ locustfile.py
@@ -0,0 +1,22 @@
+from locust import HttpUser, task, between
+import json
+
+class APITestUser(HttpUser):
+    wait_time = between(0.01, 0.1)  
+
+    @task
+    def predict(self):
+        payload = {
+            "regno_recognize": "О041ВВ797",
+            "afts_regno_ai": "О041ВВ777",
+            "recognition_accuracy": 2.25,
+            "afts_regno_ai_score": 0.924,
+            "afts_regno_ai_char_scores": "[0.97, 0.99, 0.99]",
+            "afts_regno_ai_length_scores": "[1e-9, 1e-9, 1e-9]",
+            "camera_type": "Стационарная",
+            "camera_class": "Астра-Трафик",
+            "time_check": "2021-08-07 08:10:42",
+            "direction": "1"
+        }
+        headers = {'Content-Type': 'application/json'}
+        self.client.post("/predict", data=json.dumps(payload), headers=headers)
